ef4498a8ff4e7a79085ff96f54474a29c5f3b1b0
rogerhuang <haoming5674@gmail.com>
2023-04-12 19:04:26 -0400

    added scripting for transitions

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.contact-me').classList.add('show');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.location').classList.add('show');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.Projects').classList.add('show');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.sub-header2').classList.add('show');
+});
