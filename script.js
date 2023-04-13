070bbf74e67278494663378df023d33601555cc5
rogerhuang <haoming5674@gmail.com>
2023-04-13 00:57:56 -0400

    test deployment

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.contact-me').classList.add('show');
-});
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.location').classList.add('show');
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.Projects').classList.add('show');
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.sub-header2').classList.add('show');
+    const elementsToShow = document.querySelectorAll('.contact-me, .location, .Projects, .sub-header2, .header, .contact, .sub-header1, .about');
+    elementsToShow.forEach(function(element) {
+        element.classList.add('show');
+    });
 });
